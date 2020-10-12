6d76328a18b9f16e6ba1e059b3b10ae1fead484c
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-10-12 15:03:40 +0200

    Custom Fonts

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import Home from './screens/home';
+import { AppLoading } from 'expo';
+
+const getFonts = () =>  Font.loadAsync({
+    'poppins-light': require('./assets/fonts/Poppins-Light.ttf'),
+    'poppins-regular': require('./assets/fonts/Poppins-Regular.ttf'),
+    'poppins-semibold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+});
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if(fontsLoaded){
+    return(
+      <Home />
+    );
+  } else {
+    return (
+      <AppLoading 
+        startAsync={getFonts}
+        onFinish={()=> setFontsLoaded(true)} 
+      />
+    )
+  }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ 
---
+++ screens/home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+
+export default function Home() {
+    return(
+        <View style={styles.container}>
+            <Text style={styles.titleText}>
+                Home Screen
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+    },
+    titleText: {
+        fontFamily: 'poppins-semibold',
+        fontSize: 18,
+    }
+});
