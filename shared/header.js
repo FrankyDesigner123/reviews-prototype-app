a17fdc2ac14f98526b2fe31badce8d04df2dfeb1
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-10-13 20:59:19 +0200

    background images

--- shared/header.js
+++ shared/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground} from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Dimensions } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
@@ -13,34 +13,45 @@ export default function Header(  {title} ) {
     
     
       return (
-        <View style={styles.header}>
+        <ImageBackground source={require('../assets/game_bg.png')} style={styles.header}>
           <MaterialIcons name='menu' size={28} onPress={openMenu} style={styles.icon} />
-          <View>
+          <View style={styles.headerTitle}>
+              <Image source={require('../assets/heart_logo.png')} style={styles.headerImage}/>
             <Text style={styles.headerText}>{title}</Text>
           </View>
-        </View>
+        </ImageBackground>
       );
     }
 
 const styles = StyleSheet.create({
     header: {      
-        width:'100%',
+        width:Dimensions.get('screen').width,
         height: '100%',
         flexDirection: 'row',
         flex:1,
         alignItems: 'center',
+
     },
     headerText: {
       fontWeight: 'bold',
       fontSize: 20,
       color: '#333',
       height: '100%',
+      letterSpacing: 1,
       alignItems:"center",
     },
     icon: {
       position: 'relative',
       marginLeft: 5,
       flexDirection:'column',
-      flex: 0.5
+        flex: 0.5,
+    },
+    headerImage: {
+        width: 26,
+        height: 26,
+        marginHorizontal: 10,
+    },
+    headerTitle: {
+        flexDirection: 'row',
     }
   });
