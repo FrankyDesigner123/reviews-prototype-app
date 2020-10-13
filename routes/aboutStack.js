a9e8d6eefd311542d74607c667f8f6269083530f
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-10-13 19:48:04 +0200

    custom Header Component

--- routes/aboutStack.js
+++ routes/aboutStack.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import About from '../screens/about';
+import Header from '../shared/header';
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
-export default AboutStack = () => {
-    return (
-        <Stack.Navigator screenOptions={{headerStyle:{backgroundColor: '#fef'}}}>
-            <Stack.Screen name='About' component={About} options={{title: 'About PopcornTime'}}/>
-        </Stack.Navigator>
-    )
+export default function aboutNavigator() {
+  return (
+    <Stack.Navigator initialRouteName='About'
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: '#ada074',
+      },
+      headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+    }}
+    >
+      <Stack.Screen 
+          name='About' 
+          component={About} 
+          options={() => ({ 
+            headerTitle: () => <Header title='About' />,
+          })}
+      />
+    </Stack.Navigator>
+  );
 }
--- routes/homeStack.js
+++ routes/homeStack.js
@@ -2,14 +2,33 @@ import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../screens/home';
 import ReviewDetails from '../screens/reviewDetails';
+import Header from '../shared/header';
 
 const Stack = createStackNavigator();
+const homeStack = ({ navigation }) => {
 
-export default homeStack = () => {
-    return (
-        <Stack.Navigator screenOptions={{headerStyle:{backgroundColor: '#ffe343'}}}>
-            <Stack.Screen name="Home" component={Home} options={{title: 'PopcornTime'}}/>
-            <Stack.Screen name="Details" component={ReviewDetails} options={{title: 'Review Details'}}/>
-        </Stack.Navigator> 
-    );
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor:'#ada074',
+        }
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={ () => ({ 
+          headerTitle: () => <Header  title='Home' />,
+        })}
+
+
+      />
+      <Stack.Screen name="Details" component={ReviewDetails} options={{ title: 'Details' }} />
+    </Stack.Navigator>
+  );
 };
+
+
+export default homeStack;
